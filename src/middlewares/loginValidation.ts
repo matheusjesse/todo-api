b778c64e03217db445ff1cc233afa943ccce5539
matheus jesse <matheus_jesse@hotmail.com>
2022-09-03 23:46:43 -0300

    feat: criando validação do user consultando dados no banco de dados

--- src/middlewares/loginValidation.ts
+++ src/middlewares/loginValidation.ts
@@ -6,8 +6,14 @@ export default class LoginValidation {
   constructor(private LoginService: ILoginService) { }
 
   public loginValidation = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
     const { error } = loginSchema.validate(req.body);
     if (error) return res.status(400).json({ message: error.message });
+    const user = await this.LoginService.findUser(email);
+    if (!user) return res.status(401).json({ message: 'Incorrect email or password' });
+    if (user.password !== password) {
+      return res.status(401).json({ message: 'Incorrect email or password' });
+    }
     next();
   };
 }
