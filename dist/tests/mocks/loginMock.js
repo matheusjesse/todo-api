bbadd3be55f75c56e8c8ec81805c1b7883076b76
matheus jesse <matheus_jesse@hotmail.com>
2022-09-11 06:21:31 -0300

    fix: configurando o tsconfig pra funcionar corretamente

--- dist/tests/mocks/loginMock.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loginRegisterPostMock = void 0;
-exports.loginRegisterPostMock = {
-    "userName": "Joana Magalhães",
-    "email": "Joana@hotmail.com",
-    "password": "321321321"
-};
--- dist/tests/mocks/tokenMock.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenMock = void 0;
-exports.tokenMock = {
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuYWp1bGlhQG91dGxvb2suY29tIiwidXNlck5hbWUiOiJBbmEgSnVsaWEiLCJpZCI6NSwiaWF0IjoxNjYyNzkzNjU2fQ.L9-fhZw8KmKIZZg2o-_4P8FfZUNYisj5Sb7Tqlsgoy8"
-};
